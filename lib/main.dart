0c80e795017414630ed0ed53d5ca95894b5e2e3d
rupal-mathur1 <70269196+rupal-mathur1@users.noreply.github.com>
2020-09-04 08:13:24 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,69 @@
+import 'dart:html';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-void main() {
-  runApp(MaterialApp(
-    home: Home()
-  ));
-}
-class Home extends StatelessWidget {
+void main()=> runApp( MyApp() );
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Widget titleSelection = Container(
-      padding: EdgeInsert.all(18.0),
-      child: Row(
+    Widget titleSelection = new Container(
+      padding:  const EdgeInsets.all (10.0),
+      child: new Row(
         children: <Widget> [
-          Text('About :', style: TextStyle( fontSize: 25.0)),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children:<Widget> [
-                Text('Name : Rupal Mathur  ', style: TextStyle( fontSize: 20.0)),
-                Text('From: Varanasi ', style: TextStyle( fontSize: 20.0)),
-                Text('Current Profile : B.tech 2 yr ', style: TextStyle( fontSize: 20.0)),
-                Text('Instiution : Ajay Kumar Garg Engineering College ', style: TextStyle( fontSize: 20.0)),
-                Text('Branch: IT ', style: TextStyle( fontSize: 25.0)),
-              ],
+       new Expanded(
+         child: new Column(
+           crossAxisAlignment: CrossAxisAlignment.start,
+           children: <Widget> [
+             new Container (
+               padding:  const EdgeInsets.all (10.0),
+               child: new Text('About :',
+                   style: TextStyle( fontWeight: FontWeight.bold, fontSize: 26.0)),
+             ),
+             Text('Name : Rupal Mathur  ', style: TextStyle(
+                 fontStyle: FontStyle.italic,
+                 fontSize: 20.0)
+             ),
+             Text('From: Varanasi ', style: TextStyle(
+                 fontSize: 20.0)
+             ),
+             Text('Current Profile : B.tech 2 yr ', style: TextStyle
+               ( fontSize: 20.0)
+             ),
+             Text('Instiution : Ajay Kumar Garg Engineering College ',
+                 style: TextStyle( fontSize: 20.0)
+             ),
+             Text('Branch: IT ', style: TextStyle( fontSize: 25.0)),
+             Text('A BRL Trainee ',
+                 style: TextStyle(
+                     color: Colors.indigo ,
+                     fontSize: 20.0)
+             ),
+           ],
+         ),
+       ),
+          new Icon(Icons.account_box, color: Colors.indigo ),
+          new Text( ' '),
+        ],
             ),
-          ),
-        ],
-      ),
-    );
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Basic Info'),
-        backgroundColor: Colors.black,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text('Click'),
-        backgroundColor: Colors.green,
-      ),
-      body : ListView(
-        children: <Widget> [
-         child : Image: NetworkImage('https://images.unsplash.com/uploads/14116941824817ba1f28e/78c8dff1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60')
-
-      fit: BoxFit.cover ,
           );
-      titleSelection
-        ],
-      );
-    ) ;
+    return new MaterialApp (
+      title: " ",
+      home: new  Scaffold(
+         appBar: new AppBar(
+           title: Text('Basic Info'),
+        backgroundColor: Colors.black,
+        ),
+        body: new  ListView(
+          children: <Widget> [
+           new Image.asset(
+        'Zoop-unsplash.jpg',
+          fit: BoxFit.cover
+        ),
+            titleSelection
+          ],
+      ),
+      )
+    );
   }
 }
-
